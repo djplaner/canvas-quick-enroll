18d063fe92f5de70039915a61ac6d98ebb1f27a8
David Jones <davidthomjones@gmail.com>
2022-11-08 06:28:34 +1000

    Clean up user script

--- public/canvas-quick-enroll.user.js
+++ public/canvas-quick-enroll.user.js
@@ -5,8 +5,6 @@
 // @description  Add a quick enrol button to a Canvas course site
 // @homepageURL  https://github.com/djplaner/canvas-quick-enrol
 // @supportURL   https://github.com/djplaner/canvas-quick-enrol/issues
-// @updateURL    https://github.com/7nik/test-svelte-userscript/releases/latest/download/userscript.meta.js
-// @downloadURL  https://github.com/7nik/test-svelte-userscript/releases/latest/download/userscript.user.js
 // @require      https://cdn.jsdelivr.net/gh/djplaner/canvas-quick-enroll/public/index.js
 // @author       David Jones
 // @match        https://*/courses/*
