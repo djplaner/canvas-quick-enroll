af91bd719b7a57cc90a121428d0673690605b2d0
David Jones <davidthomjones@gmail.com>
2022-11-07 20:29:59 +1000

    Releasing something useful (the wrong way?)

---
+++ dist/canvas-quick-enroll.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         canvas-quick-enrol
+// @namespace    djplaner
+// @version      0.1
+// @description  Add a quick enrol button to a Canvas course site
+// @homepageURL  https://github.com/djplaner/canvas-quick-enrol
+// @supportURL   https://github.com/djplaner/canvas-quick-enrol/issues
+// @updateURL    https://github.com/7nik/test-svelte-userscript/releases/latest/download/userscript.meta.js
+// @downloadURL  https://github.com/7nik/test-svelte-userscript/releases/latest/download/userscript.user.js
+// @require      file:///C:\Users\s2986288\code\canvas-quick-enroll\dist\index.js
+// @author       djplaner
+// @match        https://*/courses/*
+// @grant        none
+// ==/UserScript==
